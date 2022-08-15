d851cda722bd3e878683ebce02d31b89e5490f51
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-08-14 17:37:01 -0700

    Fixed 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,10 @@
 # Set up your client SSH configuration file so that you can connect to a
 # server without typing a password
 
-file_line { '/etc/ssh/ssh_config':
+file_line { 'PasswordAuthentication':
     path => '/etc/ssh/ssh_config',
-    line => '   PasswordAuthentication no'}
+    line => '   PasswordAuthentication no',}
 
-file_line { '/etc/ssh/ssh_config':
+file_line { 'IdentifyFile':
     path => '/etc/ssh/ssh_config',
-    line => '   IdentityFile ~/.ssh/school'}
+    line => '   IdentityFile ~/.ssh/school',}
