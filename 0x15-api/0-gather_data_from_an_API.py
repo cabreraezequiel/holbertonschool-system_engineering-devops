2a1ecdecbd104a00fb99d9c94f63ba8f974386db
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-09-06 12:10:36 -0700

    Fixed task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
                      (sys.argv[1]))
     user_id = r.json().get("id")
     user_name = r.json().get("name")
-    r = requests.get("https://jsonplaceholder.typicode.com/todos".format
-                     (sys.argv[1]))
+    r = requests.get("https://jsonplaceholder.typicode.com/todos")
+
     tasks = 0
     fin_tasks = 0
     for i in r.json():
@@ -19,8 +19,8 @@ if __name__ == "__main__":
             fin_tasks += 1
         if i.get("userId") == user_id:
             tasks += 1
-    print("Employee {} is done with tasks({}/{})".format(user_name,
-                                                         fin_tasks, tasks))
+    print("Employee {} is done with tasks({}/{}):".format(user_name,
+                                                          fin_tasks, tasks))
     for i in r.json():
         if i.get("userId") == user_id and i.get("completed") is True:
             print("\t {}".format(i.get("title")))
