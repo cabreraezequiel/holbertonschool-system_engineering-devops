fd2a25b03b0ff60600879736ac5afb57eabf406e
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-09-06 12:29:00 -0700

    Fixed task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -14,15 +14,8 @@ if __name__ == "__main__":
     r = requests.get("https://jsonplaceholder.typicode.com/todos")
 
     with open("{}.csv".format(user_id), 'w') as f:
-        fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED",
-                      "TASK_TITLE"]
-#        writer = csv.DictWriter(f, fieldnames=fieldnames)
-        writer = csv.writer(f)
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
         for i in r.json():
             if i.get("userId") == user_id:
                 writer.writerow([str(user_id), user_username,
                                 str(i.get("completed")), i.get("title")])
-#                writer.writerow({"USER_ID": str(user_id),
-#                                 "USERNAME": user_username,
-#                                 "TASK_COMPLETED": str(i.get("completed")),
-#                                 "TASK_TITLE": i.get("title")})
