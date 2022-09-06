59ae4e1756e4a22ba3b19759472378f63fe7840a
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-09-06 12:13:23 -0700

    Fixed task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -11,19 +11,18 @@ if __name__ == "__main__":
                      (sys.argv[1]))
     user_id = r.json().get("id")
     user_username = r.json().get("username")
-    r = requests.get("https://jsonplaceholder.typicode.com/todos".format
-                     (sys.argv[1]))
+    r = requests.get("https://jsonplaceholder.typicode.com/todos")
 
     with open("{}.csv".format(user_id), 'w') as f:
         fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED",
                       "TASK_TITLE"]
-        writer = csv.DictWriter(f, fieldnames=fieldnames)
-#        writer = csv.writer(f)
+#        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer = csv.writer(f)
         for i in r.json():
             if i.get("userId") == user_id:
-                writer.writerow({"USER_ID": str(user_id),
-                                 "USERNAME": user_username,
-                                 "TASK_COMPLETED": str(i.get("completed")),
-                                 "TASK_TITLE": i.get("title")})
-#               writer.writerow([str(user_id), user_username,
-# str(i.get("completed")), i.get("title")])
+                writer.writerow([str(user_id), user_username,
+                                str(i.get("completed")), i.get("title")])
+#                writer.writerow({"USER_ID": str(user_id),
+#                                 "USERNAME": user_username,
+#                                 "TASK_COMPLETED": str(i.get("completed")),
+#                                 "TASK_TITLE": i.get("title")})
