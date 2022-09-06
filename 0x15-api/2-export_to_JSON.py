6da48571aeba5a59b304e2ef6cf05c7f892cb554
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-09-06 12:37:55 -0700

    Tried another fix

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -13,11 +13,11 @@ if __name__ == "__main__":
     r = requests.get("https://jsonplaceholder.typicode.com/todos?userId=\
 {}".format(sys.argv[1]))
 
-    with open("{}.json".format(user_id), 'w') as f:
+    with open("{}.json".format(user_id), 'a') as f:
         user_dict_list = []
         for i in r.json():
             user_dict = {'task': i.get("title"),
                          'completed': str(i.get("completed")),
                          'username': user_username}
-            user_dict_list.append(user_dict)
+            user_dict_list += [user_dict]
         json.dump({user_id: user_dict_list}, f)
